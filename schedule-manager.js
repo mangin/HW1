0f7c247d79f267f75f9736d25b1dc71bf2db3ca3
Nazemnykh Anton <nazemnykh.anton@gmail.com>
2015-04-03 08:29:13 +0500

    Исправлена ошибка в менеджере: возвращает не расписание, а сам менеджер.

--- schedule-manager.js
+++ schedule-manager.js
@@ -4,32 +4,40 @@ function createScheduleManager(schedule) {
     var manager = {schedule: schedule};
 
     // Оставляет в расписании только сеансы со временем в диапазоне [from, to]
+    // Возвращает менеджера с изменённым расписанием
     manager.filterTime = function (from, to) {
-        return this.schedule.filter(function (scheduleItem) {
+        schedule = schedule.filter(function (scheduleItem) {
             return scheduleItem.date.value >= from.value
                     && scheduleItem.date.value <= to.value;
         });
+        return this;
     };
 
     // Оставляет в расписании только сеансы в кинотеатрах, перечисленных в массиве theaters
+    // Возвращает менеджера с изменённым расписанием
     manager.filterTheaters = function (theaters) {
-        return this.schedule.filter(function (scheduleItem) {
+        schedule = schedule.filter(function (scheduleItem) {
             return theaters.some(function (theater) {
                 return scheduleItem.theater === theater;
             });
         });
+        return this;
     };
 
     // Оставляет в расписании только сеансы с фильмами, перечисленными в массиве films
+    // Возвращает менеджера с изменённым расписанием
     manager.filterFilms = function (films) {
-        return this.schedule.filter(function (scheduleItem) {
+        schedule = schedule.filter(function (scheduleItem) {
             return films.some(function (film) {
                 return scheduleItem.film === film;
             });
         });
+        return this;
     };
 
     return manager;
 }
 
-
+//// Пример использования:
+//manager = createScheduleManager(schedule);
+//manager.
